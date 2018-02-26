859cd5d38d8f804098369791e873217ca7e0fbbe
Mirko Nasato <mirko@encoded.io>
2018-02-26 22:58:04 +0000

    updated neo4j to v3.2.9 and adjusted the code to work with it

--- graphipedia-dataimport/src/main/java/org/graphipedia/dataimport/neo4j/ImportGraph.java
+++ graphipedia-dataimport/src/main/java/org/graphipedia/dataimport/neo4j/ImportGraph.java
@@ -21,6 +21,8 @@
 //
 package org.graphipedia.dataimport.neo4j;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,9 +34,8 @@ public class ImportGraph {
     private final BatchInserter inserter;
     private final Map<String, Long> inMemoryIndex;
 
-    public ImportGraph(String dataDir) {
-        inserter = BatchInserters.inserter(dataDir);
-        inserter.createDeferredSchemaIndex(WikiLabel.Page).on("title").create();
+    public ImportGraph(String dataDir) throws IOException {
+        inserter = BatchInserters.inserter(new File(dataDir));
         inMemoryIndex = new HashMap<String, Long>();
     }
 
@@ -71,6 +72,7 @@ public class ImportGraph {
     }
 
     public void finish() {
+        inserter.createDeferredSchemaIndex(WikiLabel.Page).on("title").create();
         inserter.shutdown();
     }
 
